f3c5bb2b4a3d8a85c5b76ea457c14b5e314e2a08
pm990320 <patmenlove@gmail.com>
2015-01-11 20:11:20 +0000

    prova queste 'sfide'...

---
+++ search.py
@@ -0,0 +1,23 @@
+# Search (Ricerca)
+
+# Immaginiamo che hai una lista di elementi qualsiasi
+# Scrivi una function che prende come parametro una lista dove cercare e un valore da cercare, 
+# che ritorna l'indice di dove si trova quel valore.
+
+# Se non si trova, la function deve ritornare 
+False
+
+# come lo fai dipende da te... pero, si come e difficile, ti daro degli indizi:
+
+
+#1 Prima di tutto devi controllare ogni elemento di una lista, quindi serve un tipo di ciclo......
+
+#2 Poi ricorda che per usare l'operatore ==  bisogna che il lato destro sia dello stesso tipo del
+# lato sinistro. Quindi se ricerchiamo una string, dovvrai utilizzare la function
+str()
+
+#3 Non farlo troppo complicato, e piu semplice che pensi. Non usare classi, o eccezioni.
+
+
+
+# Quando l'hai completato, fammi vedere che capolavoro hai fatto :P
